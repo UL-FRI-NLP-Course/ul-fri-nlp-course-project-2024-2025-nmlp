cb40a477f9306071723e44a0082ae9b36c6e36f8
jan146 <anurjan@gmail.com>
2025-05-01 14:35:19 +0200

    Load all sheets

--- src/main.py
+++ src/main.py
@@ -3,7 +3,11 @@ import pandas as pd
 INPUT_DATA_PATH: str = "data/Podatki - PrometnoPorocilo_2022_2023_2024.xlsx"
 
 def main():
-    df: pd.DataFrame = pd.read_excel(INPUT_DATA_PATH)
+    df: pd.DataFrame = pd.DataFrame()
+    sheet_to_df: dict[str, pd.DataFrame] = pd.read_excel(INPUT_DATA_PATH, sheet_name=None)
+    for sheet_name, sheet_df in sheet_to_df.items():
+        sheet_df["sheet_name"] = sheet_name
+        df = pd.concat([df, sheet_df])
 
 if __name__ == "__main__":
     main()
