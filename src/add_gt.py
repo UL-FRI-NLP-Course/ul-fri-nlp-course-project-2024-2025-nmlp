ed2ef032edc87c98d49c97f4d364fc357c4e81cd
anzehocevar <anze.hocevar@gmail.com>
2025-05-30 22:18:23 +0200

    added utf8 encoding and gt for evaluation

--- src/add_gt.py
+++ src/add_gt.py
@@ -5,21 +5,27 @@ OUTPUTS_PATH: str = "data/dp1_outputs.jsonl"
 NEW_FILE: str = "data/dp1_gt.jsonl"
 
 def main():
-    with open(INPUTS_PATH, "rt") as file:
-        inputs_lines: list[str] = file.readlines()
-        inputs: list[dict] = [json.loads(line) for line in inputs_lines]
-    with open(OUTPUTS_PATH, "rt") as file_src, open(NEW_FILE, "wt") as file_dst:
-        for line in file_src.readlines():
+    # read inputs as UTF-8
+    with open(INPUTS_PATH, "rt", encoding="utf-8") as file:
+        inputs = [json.loads(line) for line in file]
+        
+    # read outputs as UTF-8, write new file as UTF-8
+    with open(OUTPUTS_PATH, "rt", encoding="utf-8") as file_src, \
+         open(NEW_FILE,     "wt", encoding="utf-8") as file_dst:
+
+        for line in file_src:
             obj = json.loads(line)
-            gt = None
-            for obj_ in inputs:
-                if obj_["text"] == obj["input"]:
-                    gt = obj_["gt"]
-            if gt:
-                obj["gt"] = gt
+            # find matching gt
+            for inp in inputs:
+                if inp["text"] == obj["input"]:
+                    obj["gt"] = inp["gt"]
+                    break
+
+            if "gt" in obj:
                 file_dst.write(json.dumps(obj, ensure_ascii=False) + "\n")
             else:
-                print("Failed to find gt for line: " + line)
+                print("Failed to find gt for line:", obj["input"])
+
 
 if __name__ == "__main__":
     main()
