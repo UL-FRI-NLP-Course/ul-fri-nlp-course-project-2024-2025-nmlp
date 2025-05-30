9397032070b8dcffa19f28098b89c52f49d80513
ja8769 <ja8769@gwn09.arnes.si>
2025-05-30 18:45:22 +0200

    DP1 inference from jsonl file

--- fine_tunning/dp1_inf.py
+++ fine_tunning/dp1_inf.py
@@ -1,6 +1,13 @@
 import torch
+import time
+import json
+import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from peft         import PeftConfig, PeftModel
+from evaluation.llm_evaluation_2 import GENERATION_INSTRUCTIONS
+
+INPUTS_PATH: str = "data/dp1_inputs.jsonl"
+OUTPUT_PATH: str = "data/dp1_outputs.jsonl"
 
 # Path to your adapter
 # PEFT_DIR = "/d/hpc/projects/onj_fri/peft_ah/2b-dp1"
@@ -17,37 +24,6 @@ model_to_dtype: dict[str, torch.dtype] = {
     "cjvt/GaMS-9B-Instruct": torch.bfloat16,
     "cjvt/GaMS-27B-Instruct": torch.bfloat16,
 } 
-
-# 1) Load the adapter config to discover the base model name
-config = PeftConfig.from_pretrained(PEFT_DIR)
-base_name = config.base_model_name_or_path
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-# 2) Load tokenizer & base model
-tokenizer = AutoTokenizer.from_pretrained(base_name)
-model     = AutoModelForCausalLM.from_pretrained(
-    base_name,
-    device_map=device,
-    torch_dtype=model_to_dtype[model_id],
-)
-
-# 3) Wrap the base model with your PEFT adapter
-model = PeftModel.from_pretrained(
-    model, PEFT_DIR,
-    device_map=device,
-    torch_dtype=model_to_dtype[model_id],
-)
-
-# pline = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
-pline = pipeline(
-    "text-generation",
-    model=model,
-    device_map=device,
-    torch_dtype=model_to_dtype[model_id],
-    tokenizer=tokenizer,
-)
-
 
 shot_1: str = """
     VHOD:
@@ -118,83 +94,51 @@ shot_3: str = """
     # "The examples of antonyms are:\nhigh => low\nwide => narrow\nbig =>",
     # "Pristanek je bil prvi nadzorovani spust ameriškega vesoljskega plovila na površje Lune po Apollu 17 leta 1972, ko je na Luni pristala zadnja Nasina misija s posadko.\nDoslej so na Luni pristala vesoljska plovila le iz štirih drugih držav –",
     # "U četvrtak je bila prva polufinalna večer Dore, a komentari na društvenim mrežama ne prestaju. U nedjeljno finale prošli su:",
-prompts = [
-    f"""
-    Poziv za generiranje prometnih poročil (RTV Slovenija)
-    Generiraj kratko in strukturirano prometno poročilo v slogu RTV Slovenija na podlagi tabelaričnih prometnih dogodkov (iz preteklih X ur, npr. 3–4 ure).
-    Oblika poročila:
-    ------------------------------------------------------------
-    Prometne informacije       DD. MM. YYYY       HH.MM           2. program
-    
-    Podatki o prometu.
-    
-    <sledi povzetek> 
-    ------------------------------------------------------------
-    
-    Navodila za vsebino:
-    - Povzemi pomembne dogodke: nesreče, zapore cest, živali na vozišču, izredne razmere (burja, tovorna prepoved).
-    - Vključi strukturo stavkov:
-        1. opcija: Cesta in smer + razlog + posledica + odsek
-        2. opcija: Razlog + cesta in smer + posledica + odsek
-    - Prednost imajo stavki, ki vključujejo:
-      - znana cestna imena (avtoceste, razcepi, mesta),
-      - konkretne učinke na promet (zastoji, zapora pasu),
-      - točne lokacije (med priključkoma X in Y).
-    
-    Navodila za slog:
-    - Ne ponavljaj istih ali zelo podobnih informacij.
-    - Združi podobne stavke, obdrži najbolj informativen.
-    - Ne vključuj trivialnih informacij ali običajnih konic, razen če so izredno dolge ali nenavadne.
-    - Vsak stavek naj bo jedrnat in razumljiv za branje v etru.
-    
-    Poročilo se mora končati z oznako <EOS>
-    Spodaj sledijo primeri vhodnih podatkov (iz Excela) in ustrezno oblikovanih izhodnih poročil:
 
-    {shot_1}
+prompt_template = f"""
+Si profesionalen poročevalec prometnih informacij.
+Dobil boš nekaj podatkov, ki so bili pridobljeni s spletne strani prometnih informacij.
+Tvoja naloga je da ustvariš kratko prometno poročilo (IZHOD) na podlagi teh podatkov (VHOD).
+Poročilo se mora končati z oznako <EOS>
 
-    ### NASLEDNJI PRIMER ###
+Oblika poročila:
+------------------------------------------------------------
+Prometne informacije       DD. MM. YYYY       HH.MM           2. program
 
-    {shot_2}
+Podatki o prometu.
 
-    ### NASLEDNJI PRIMER ###
+<sledi povzetek> 
+------------------------------------------------------------
 
-    {shot_3}
+Slediti moraš naslednjim navodilom:
 
-    ### NASLEDNJI PRIMER ###
+### ZAČETEK NAVODIL
 
-    VHOD:
-    {{input}}
+{GENERATION_INSTRUCTIONS}
 
-    IZHOD:
-    """,
-    f"""
-    Si profesionalen poročevalec prometnih informacij.
-    Dobil boš nekaj podatkov, ki so bili pridobljeni s spletne strani prometnih informacij.
-    Tvoja naloga je da ustvariš kratko prometno poročilo (IZHOD) na podlagi teh podatkov (VHOD).
-    Poročilo se mora končati z oznako <EOS>
+### KONEC NAVODIL
 
-    Nadaljuj zaporedje naslednjih primerov:
+Nadaljuj zaporedje naslednjih primerov:
 
-    {shot_1}
+{shot_1}
 
-    ### NASLEDNJI PRIMER ###
+### NASLEDNJI PRIMER ###
 
-    {shot_2}
+{shot_2}
 
-    ### NASLEDNJI PRIMER ###
+### NASLEDNJI PRIMER ###
 
-    {shot_3}
+{shot_3}
 
-    ### NASLEDNJI PRIMER ###
+### NASLEDNJI PRIMER ###
 
-    VHOD:
-    {{input}}
+VHOD:
+{{input}}
 
-    IZHOD:
-    """,
-]
+IZHOD:
+""".strip()
 
-input: str = """
+example_input: str = """
 Prometne informacije       09. 09. 2024      19.30           2. program
 
 Podatki o prometu.
@@ -203,26 +147,84 @@ Nesreče Na podravski avtocesti je na razcepu Draženci iz smeri Ptuja proti Hrv
 
 """
 
-prompts = [prompt.format(input=input) for prompt in prompts]
+def test():
+    prompt = prompt_template.format(input=example_input)
+    sequences = pline(
+        [prompt],
+        max_new_tokens=512,
+        num_return_sequences=1,
+        return_full_text=False,
+    )
+    for seq in sequences:
+        result: str = seq[0]["generated_text"]
+        try:
+            result = result[:result.index("<EOS>")]
+        except:
+            print("Error while trying to cut generated output")
+    
+        print("--------------------------")
+        print(f"Result: {result}")
+        print("--------------------------\n")
 
-sequences = pline(
-    prompts,
-    max_new_tokens=512,
-    num_return_sequences=1,
-    return_full_text=False,
-)
+def main():
+    lines_to_skip = 0
+    with open(OUTPUT_PATH, "rt") as file:
+        lines_to_skip = len(file.readlines())
+    print(f"Lines to skip: {lines_to_skip}")
+    df: pd.DataFrame = pd.read_json(INPUTS_PATH, lines=True)
+    example_inputs: list[str] = [x for x in df["text"]]
+    example_inputs = example_inputs[lines_to_skip:]
+    prompts: list[str] = [prompt_template.format(input=example) for example in example_inputs]
+    with open(OUTPUT_PATH, "at") as file:
+        for i in range(len(prompts)):
+            print(f"[{i+1}/{len(prompts)}]")
+            t0 = time.time()
+            sequences = pline(
+                [prompts[i]],
+                max_new_tokens=512,
+                num_return_sequences=1,
+                return_full_text=False,
+            )
+            try:
+                seq = sequences[0]
+                result: str = seq[0]["generated_text"]
+                result = result[:result.index("<EOS>")]
+                file.write(json.dumps({"input": example_inputs[i], "output": result}, ensure_ascii=False) + "\n")
+                file.flush()
+            except Exception as e:
+                print("Error while trying to save generated output: " + str(e))
+            print(f"Took {time.time()-t0:.1f} seconds")
+    print("Done")
 
-
-for seq in sequences:
-    result: str = seq[0]["generated_text"]
-    try:
-        result = result[:result.index("<EOS>")]
-    except:
-        print("Error while trying to cut generated output")
-
-    print("--------------------------")
-    print(f"Result: {result}")
-    print("--------------------------\n")
-
-
+if __name__ == "__main__":
+    # 1) Load the adapter config to discover the base model name
+    config = PeftConfig.from_pretrained(PEFT_DIR)
+    base_name = config.base_model_name_or_path
+    
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    
+    # 2) Load tokenizer & base model
+    tokenizer = AutoTokenizer.from_pretrained(base_name)
+    model     = AutoModelForCausalLM.from_pretrained(
+        base_name,
+        device_map=device,
+        torch_dtype=model_to_dtype[model_id],
+    )
+    
+    # 3) Wrap the base model with your PEFT adapter
+    model = PeftModel.from_pretrained(
+        model, PEFT_DIR,
+        device_map=device,
+        torch_dtype=model_to_dtype[model_id],
+    )
+    
+    # pline = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
+    pline = pipeline(
+        "text-generation",
+        model=model,
+        device_map=device,
+        torch_dtype=model_to_dtype[model_id],
+        tokenizer=tokenizer,
+    )
+    main()
 
